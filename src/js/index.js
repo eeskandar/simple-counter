f7e10d88a61557bc9e56ffc1aa3374eac52fa221
Iskandar <seik747@gmail.com>
2022-08-23 19:57:00 +0000

    setting the interval for our Clock function

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,39 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Clock } from "./component/Clock.jsx";
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let hours1st = 0;
+let hours2nd = 0;
+let minutes1st = 0;
+let minutes2nd = 0;
+let seconds1st = 0;
+let seconds2nd = 0;
+
+setInterval(()=>{
+    seconds1st++
+    if(seconds1st == 10){
+        seconds1st = 0
+        seconds2nd++
+        if(seconds2nd == 6){
+            seconds2nd = 0
+            minutes1st++
+            if(minutes1st == 10){
+                minutes1st = 0
+                minutes2nd++
+                if(minutes2nd == 6){
+                    minutes2nd = 0
+                    hours1st++
+                    if(hours1st == 10){
+                        hours1st = 0
+                        hours2nd++
+                    }
+                }
+            }
+        }
+    }
+    ReactDOM.render(<Clock hours2nd ={hours2nd} hours1st={hours1st} minutes2nd={minutes2nd} minutes1st={minutes1st} seconds2nd={seconds2nd} seconds1st={seconds1st}/>, document.querySelector("#app"));
+}, 1000)
+
